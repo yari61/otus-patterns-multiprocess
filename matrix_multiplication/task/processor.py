4f6f496e55b075bd665ec7be0ae64a2f6ea20d34
Yaroslav Borysiuk <yaroslav.borysiuk@lifecell.com.ua>
2021-01-17 14:07:02 +0200

    tasks to method parameters

--- matrix_multiplication/task/processor.py
+++ matrix_multiplication/task/processor.py
@@ -1,3 +1,6 @@
+"""Module with concrete implementation of task processor
+"""
+from __future__ import annotations
 import typing
 import multiprocessing
 
@@ -5,15 +8,14 @@ from matrix_multiplication.abc.task import TaskProcessor, Task
 
 
 class MultiprocessTaskProcessor(TaskProcessor):
-    __slots__ = ("_pool", "_tasks")
+    __slots__ = ("_pool",)
 
-    def __init__(self, pool: multiprocessing.Pool, tasks: typing.Iterable[Task]):
+    def __init__(self, pool: multiprocessing.Pool):
         self._pool = pool
-        self._tasks = tasks
 
-    def __call__(self) -> typing.Iterable[object]:
+    def __call__(self, tasks: typing.Iterable[Task]) -> typing.Iterable[object]:
         # here tasks are spread between a pool of workers (processes)
-        tasks = [self._pool.apply_async(task) for task in self._tasks]
+        tasks = [self._pool.apply_async(task) for task in tasks]
         # waiting for tasks completion
         results = [task.get() for task in tasks]
         return results
