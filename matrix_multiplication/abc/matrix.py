d223738e5b7d366d3c39f3034fd48c33e4fb273e
Yaroslav Borysiuk <yaroslav.borysiuk@lifecell.com.ua>
2021-01-17 14:03:15 +0200

    MutableMatrix

--- matrix_multiplication/abc/matrix.py
+++ matrix_multiplication/abc/matrix.py
@@ -1,12 +1,16 @@
+"""Module with abstract classes required for matrix-like objects
+"""
+from __future__ import annotations
 import abc
 import typing
+from typing import Iterable, List, Tuple
 
 
 class LeftMultipliableMatrix(abc.ABC):
     """Left matrix in multiplication
     """
     @abc.abstractmethod
-    def get_row(self, index: int) -> typing.Iterable[typing.SupportsFloat]:
+    def get_row(self, index: int) -> Iterable[float]:
         pass
 
     @abc.abstractmethod
@@ -18,7 +22,7 @@ class RightMultipliableMatrix(abc.ABC):
     """Right matrix in multiplication
     """
     @abc.abstractmethod
-    def get_column(self, index: int) -> typing.Iterable[typing.SupportsFloat]:
+    def get_column(self, index: int) -> Iterable[float]:
         pass
 
     @abc.abstractmethod
@@ -29,3 +33,13 @@ class RightMultipliableMatrix(abc.ABC):
 class ABCMatrix(LeftMultipliableMatrix, RightMultipliableMatrix):
     """Matrix abstract class
     """
+
+
+class ABCMutableMatrix(ABCMatrix):
+    @abc.abstractmethod
+    def append(self, row: List[float]) -> None:
+        pass
+
+    @abc.abstractmethod
+    def reshape(self, new_shape: Tuple[int, int]) -> None:
+        pass
